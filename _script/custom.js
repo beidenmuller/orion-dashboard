b5023f831a7f1644524621c94b45707487dfcf5f
beidenmuller <bill.eidenmuller@gmail.com>
2017-12-07 10:43:24 -0500

    comment out unused js function

--- _script/custom.js
+++ _script/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 });
 
 function initDynamicContent(container) {
-    initDynamicForm(container);
+    //initDynamicForm(container);
 
     bindDynamicTabs(container);
 
