11440a2a377bdd3b05f3e791352f9d65e47894de
beidenmuller <34341296+beidenmuller@users.noreply.github.com>
2017-12-08 11:07:16 -0500

    Update custom.js

--- _script/custom.js
+++ _script/custom.js
@@ -1,14 +1,40 @@
 ﻿
-$(document).ready(function () {
-    bindDynamicTabs();
-    
-    initDynamicLinks();
-});
+jQuery(document).on(
+    "pageshow", 
+    "[data-role='page']", 
+    function(e,ui){				
+        //.ui-page-active
 
-function initDynamicContent(container) {
+        jQuery(document).one("dialogcreate", enableCloseOnFocusOut);
+
+        function dlgClickOffClose(e,ui){ jQuery(e.target).dialog("close"); }
+
+        function stopPropagation(e,ui){ e.stopPropagation(); }
+
+        function enableCloseOnFocusOut(e,ui){
+            var d = jQuery(e.target);
+            var c = d.find(".ui-dialog-contain");
+
+            if(c.length){ c.off("click", stopPropagation).on("click", stopPropagation); }
+
+            d.off("click", dlgClickOffClose).on("click", dlgClickOffClose);
+        }
+    }
+);
+
+
+
+
+//$(document).ready(function () {
+    //bindDynamicTabs();
+    
+    //initDynamicLinks();
+//});
+
+//function initDynamicContent(container) {
     //initDynamicForm(container);
 
-    bindDynamicTabs(container);
+    //bindDynamicTabs(container);
 
-    initDynamicLinks(container);
-}
+    //initDynamicLinks(container);
+//}
